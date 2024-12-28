5b3774088e7d40db4d9289bddcd823c23972468b
Sujal Kumar Sinha <sujalksinha23@gmail.com>
2024-12-28 11:28:17 +0530

    add tasks and delete task ui prototype

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 console.log("testing");
 addTaskButton = document.getElementById("add-task");
 input = document.getElementById("input");
+taskText = document.getElementById("task-text");
+tasks = [];
 
+//handle the click event on the add task button
 addTaskButton.addEventListener("click", function (e) {
   e.preventDefault();
+  taskText.innerHTML = input.value;
   console.log("e.value", input.value);
+  input.value = "";
 });
+
+//delete task button event handling
+deleteTaskButton = document.getElementById("delete-button");
+
+deleteTaskButton.addEventListener("click", function (e) {});
